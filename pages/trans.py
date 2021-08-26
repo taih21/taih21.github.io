fed9eb304a8299ac9f2bcd55a6c7e94b7a3270ff
taih21 <89570707+taih21@users.noreply.github.com>
2021-08-26 00:21:17 -0700

    Add files via upload

---
+++ pages/trans.py
@@ -0,0 +1,25 @@
+import xml
+from xml.dom.minidom import parse
+
+
+domTree = parse('CameraReadyPapers.xml')
+root = domTree.documentElement
+rows = root.getElementsByTagName('Row')
+with open('tmp.txt','w',encoding='utf-8') as f:
+    for i in range(len(rows)):
+        if i == 0 or i == 1:
+            continue
+        prefix1 = "<li><b><u><a href=>"
+        prefix2 = "</a></u></b> - <i>"
+        prefix3 = "</i></li>"
+
+        title = rows[i].getElementsByTagName('Data')[1].firstChild.data
+        author = rows[i].getElementsByTagName('Data')[3].firstChild.data.replace(';',',').replace('*','')
+        # author_list = author.split(';')
+        # author = ''
+        # for j in range(len(author_list)):
+        #     author += author_list[j].split(' (')[0].split(' ')[-1] + ', '
+        # author = author[:-2]
+
+        text = prefix1 + title + prefix2 + author + prefix3 + '\n'
+        f.write(text)
